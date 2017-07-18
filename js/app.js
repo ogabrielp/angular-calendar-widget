227cd092a0eec20b1a163eac69e932e46c515487
Gabriel Pinheiro <gabrielpcosta@gmail.com>
2017-07-18 19:20:26 -0300

    Fix to work with npm

--- js/app.js
+++
@@ -1,7 +0,0 @@
-var myApp = angular.module('calendarWidget', ['angularFlatCalendar']);
-
-myApp.controller('MainController', function($scope) {
-  $scope.acw_weekdays_short = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
-  $scope.acw_month_names = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
-  'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-});
